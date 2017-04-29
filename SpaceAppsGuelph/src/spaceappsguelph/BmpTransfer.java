1128fe7e9297d4be960b49dd1fbb30e879a14d85
Alejandro <Alejandro@DESKTOP-I1TUGDE>
2017-04-29 01:16:53 -0400

    BmpTransfer is in icebox for now, moving onto developing algorithm for number detection

--- SpaceAppsGuelph/src/spaceappsguelph/BmpTransfer.java
+++ SpaceAppsGuelph/src/spaceappsguelph/BmpTransfer.java
@@ -5,7 +5,11 @@
  */
 
 package spaceappsguelph;
-
+import java.io.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.renderable.ParameterBlock;
+import javax.imageio.*;
+import java.net.URL;
 /**
  *
  * @author Loveys
@@ -16,6 +20,53 @@ public class BmpTransfer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        FileSeekableStream stream = new FileSeekableStream("sas");
+        TIFFDecodeParam decodeParam = new TIFFDecodeParam();
+        decodeParam.setDecodePaletteAsShorts(true);
+        ParameterBlock params = new ParameterBlock();
+        params.add(stream);
+        RenderedOp image1 = JAI.create("tiff", params);
+        BufferedImage img = image1.getAsBufferedImage();
+        
+        
+        
+        
+        
+        
+        URL tif;
+        try
+        {
+            tif = new URL("ftp://ftp.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/AlouetteData/500/Image0001.tif");
+        }
+        catch (Exception e)
+        {
+            System.out.println("boo!");
+        }
+        BufferedImage image = ;*/
     }
+    
+    /*public static String convert (URL inputFile)
+    {
+        BufferedImage tif = null;
+        try
+        {
+            tif = ImageIO.read(inputFile);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Image not found");
+            return("FAILURE");
+        }
+        try
+        {
+            ImageIO.write(tif, "bmp", new File("temp.bmp"));
+        }
+        catch (Exception f)
+        {
+            System.out.println("ERROR WRITING TO FILE");
+            return ("FAILURE");
+        } 
+        return ("temp.bmp");
+    }*/
 }
